45dfbd95f992fc3ce5bf4368dfeb51d378a99722
Anton111111 <anton.potekhin@gmail.com>
2015-07-12 11:37:35 +0300

    Xperia don't have LocalGroups class. Therefore we check is it exists

--- src/com/android/mms/data/Group.java
+++ src/com/android/mms/data/Group.java
@@ -172,6 +172,14 @@ public class Group {
         }
         cursor.close();
 
+        //Check if LocalGroups is exists. We have to do it because Xperia don't have this class in framework base
+        try {
+            Class.forName("android.provider.LocalGroups");
+        } catch( ClassNotFoundException e ) {
+            Log.i(TAG, "LocalGroups isn't exist");
+            return groups;
+        }
+
         final Cursor localCursor = context.getContentResolver().query(LocalGroups.CONTENT_URI,
                 LOCAL_PROJECTION, LOCAL_SELECTION, null, LOCAL_SORT);
 
@@ -192,4 +200,6 @@ public class Group {
 
         return groups;
     }
+
+    
 }
